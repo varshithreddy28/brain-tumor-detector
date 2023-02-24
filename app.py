32e524d68b61a5c1feabad9e559d52034d84416e
Varshith Reddy <varshithreddybobbili28@gmail.com>
2023-02-25 00:55:22 +0530

    updated debugging

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, redirect, render_template, request
 from keras.models import load_model
 import numpy as np
 import cv2
+import os
+from werkzeug.utils import secure_filename
+# from tensorflow.keras.preprocessing import image
+# from PIL import Image
+import keras.utils as ku
 
 # TO run this script use python -m flask ru
 
@@ -19,12 +24,19 @@ def home():
 def predict():
     if request.method == 'POST':
         photo = request.files['brainPic']
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'static', secure_filename("brain.jpg"))
+
         print("Before saving")
-        photo.save('static/brain.jpg')
+        photo.save(file_path)
         print("After Saving")
-        photo = cv2.resize(cv2.imread('static/brain.jpg'),
+        brainImg = ku.load_img(file_path)
+        print(".............................................................")
+        print(brainImg)
+        photo = cv2.resize(cv2.imread(file_path),
                            dsize=(240, 240), interpolation=cv2.INTER_CUBIC)
-        cv2.imwrite('static/brain.jpg', photo)
+        cv2.imwrite(file_path, photo)
         pred = np.round(model.predict(np.array([photo / 255.]))[0][0])
         if pred > 0.8:
             output = 'Brain Tumor Detected'
